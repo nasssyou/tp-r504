98de7f855c17e82438f94a88caff2019d3bcbe52
nasssyou <nassim.mehloul@univ-rouen.fr>
2023-09-28 15:42:06 +0200

    maj

--- TP1/test.py
+++ TP1/test.py
@@ -2,9 +2,10 @@ import pytest
 import fonctions as f
 
 def test_1():
-	assert f.puiss(2,3) == 6
-	assert f.puiss(2,2) == 2
+	assert f.puissance(2,3) == 8
+	assert f.puissance(2,2) == 4
+
 	
 def test_2( ) :
-	assert f.puissance (-2, -3) == -6
-	assert f.puissance (-2, -2) == -2
+	assert f.puissance (-2, -3) == -1./8.
+	assert f.puissance (-2, -2) == 0.25
